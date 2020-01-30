ff2d3a90537b4a394df3982c2bdf7b77f7cdb1a2
Prathmesh Patil <prathameshpatil0182@gmail.com>
2020-01-30 18:30:43 +0530

    Reactc admin build upload

---
+++ precache-manifest.c436e129491c92d29e2d3338c39c3d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5779416a8871e8904b3eac5bf324a090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9792a457a0970b94a7c9",
+    "url": "/static/js/2.9266b007.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9266b007.chunk.js.LICENSE"
+  },
+  {
+    "revision": "423b882bd7f2819ec735",
+    "url": "/static/js/main.caa610f7.chunk.js"
+  },
+  {
+    "revision": "39b52840409ec8ea3790",
+    "url": "/static/js/runtime-main.6237dbe6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
